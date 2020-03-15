c65e4477e98c0ab9b8fd10462708005647e91fd8
wsherby <wsherby@gmail.com>
2020-03-15 12:26:02 -0600

    config env settings set up

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+
+basdir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = "this-should-be-changed-please"
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEBUG = True
+    DEVELOPMENT = True
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    DEVELOPMENT = True
+
+
+class TestingConfig(Config):
+    TESTING = True
