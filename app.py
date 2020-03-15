a7a22052a5260536046acb6534d65320160582f7
wsherby <wsherby@gmail.com>
2020-03-15 12:13:01 -0600

    name route

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/<name>")
+def hello_name(name):
+    return f"Hello {name}!"
 
 
 if __name__ == "__main__":
