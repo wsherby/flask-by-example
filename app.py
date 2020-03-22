55058c7cf7699d95e8ae96b1134e0fd7e1dec539
wsherby <wsherby@gmail.com>
2020-03-22 11:36:57 -0600

    asynchronus worker queue

--- app.py
+++ app.py
@@ -3,64 +3,100 @@ import requests
 import operator
 import re
 import nltk
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from stop_words import stops
 from collections import Counter
 from bs4 import BeautifulSoup
+from rq import Queue
+from rq.job import Job
+from worker import conn
 
 app = Flask(__name__)
 app.config.from_object(os.environ["APP_SETTINGS"])
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-from models import Result
+q = Queue(connection=conn)
+
+from models import *
+
+
+def count_and_save_words(url):
+    """Use beautifulsoup to get url text, count words, and save to database"""
+    errors = []
+
+    try:
+        r = requests.get(url)
+    except:
+        errors.append("Unable to get URL. Please make sure it's valid and try again.")
+        return {"error": errors}
+
+    # text processing
+    raw = BeautifulSoup(r.text, "html.parser").get_text()
+    nltk.data.path.append("./nltk_data/")  # set the path
+    tokens = nltk.word_tokenize(raw)
+    text = nltk.Text(tokens)
+
+    # remove punctuation, count raw words
+    nonPunct = re.compile(".*[A-Za-z].*")
+    raw_words = [w for w in text if nonPunct.match(w)]
+    raw_word_count = Counter(raw_words)
+
+    # stop words
+    no_stop_words = [w for w in raw_words if w.lower() not in stops]
+    no_stop_words_count = Counter(no_stop_words)
+
+    # save the results
+    try:
+        result = Result(
+            url=url,
+            result_all=raw_word_count,
+            result_no_stop_words=no_stop_words_count,
+        )
+        db.session.add(result)
+        db.session.commit()
+        return result.id
+    except:
+        errors.append("Unable to add item to database.")
+        return {"error": errors}
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    errors = []
-    results = []
+    results = {}
     if request.method == "POST":
-        # get url that the user submitted
-        try:
-            url = request.form["url"]
-            r = requests.get(url)
-            # print(r.text)
-            # return render_template_string(r.text)
-        except:
-            errors.append(
-                "Unable to get URL. Please make sure it's valid and try again."
-            )
-            return render_template("index.html", errors=errors)
-        if r:
-            # text processing
-            raw = BeautifulSoup(r.text, "html.parser").get_text()
-            nltk.data.path.append("./nltk_data/")  # set the path
-            tokens = nltk.word_tokenize(raw)
-            text = nltk.Text(tokens)
-            # remove punctuation, count raw words
-            nonPunct = re.compile(".*[A-Za-z].*")
-            raw_words = [w for w in text if nonPunct.match(w)]
-            raw_word_count = Counter(raw_words)
-            # stop words
-            no_stop_words = [w for w in raw_words if w.lower() not in stops]
-            no_stop_words_count = Counter(no_stop_words)
-            # save the results
-            results = sorted(
-                no_stop_words_count.items(), key=operator.itemgetter(1), reverse=True
-            )[:10]
-            try:
-                result = Result(
-                    url=url,
-                    result_all=raw_word_count,
-                    result_no_stop_words=no_stop_words_count,
-                )
-                db.session.add(result)
-                db.session.commit()
-            except:
-                errors.append("Unable to add item to database.")
-    return render_template("index.html", errors=errors, results=results)
+        # this import solves a bug in rq
+        from app import count_and_save_words
+
+        # get url from form submission
+        url = request.form["url"]
+        if not url[:8].startswith(("https://", "http://")):
+            url = "http://" + url
+
+        job = q.enqueue_call(func=count_and_save_words, args=(url,), result_ttl=5000)
+
+        print(job.get_id())
+
+    return render_template("index.html", results=results)
+
+
+@app.route("/results/<job_key>", methods=["GET"])
+def get_results(job_key):
+
+    job = Job.fetch(job_key, connection=conn)
+
+    if job.is_finished:
+        result = Result.query.filter_by(id=job.result).first()
+        results = sorted(
+            result.result_no_stop_words.items(),
+            key=operator.itemgetter(1),
+            reverse=True,
+        )[:10]
+        print(results)
+        return jsonify(results)
+    else:
+        return "Nay!", 202
 
 
 if __name__ == "__main__":
